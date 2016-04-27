8791b8b3354d953f384df7c338d4c6974d674c32
goldenzs <goldenzs@plu.edu>
2016-04-27 11:09:04 -0700

    Standard is superclass now

--- farkle-client/src/main/java/edu/plu/cs/farkle/client/newGameplay.java
+++ farkle-client/src/main/java/edu/plu/cs/farkle/client/newGameplay.java
@@ -1,8 +1,8 @@
 package edu.plu.cs.farkle.client;
+
 import java.util.ArrayList;
 
-public class Standard{
-	
+public class newGameplay extends Standard{
 	protected int score = 0;
 	protected int tempScore = 0;
 	protected int oneScore = 0;
@@ -10,49 +10,6 @@ public class Standard{
 	protected static ArrayList<Integer> single = new ArrayList<Integer>();
 	
 	boolean change = false;
-	
-	/**
-	 * Add a value of a dice to the single ArrayList to be banked
-	 * @param value int face value of die to be added
-	 */
-	public void addToSingle(int value){
-		single.add(value);
-	}
-
-	
-	/**
-	 * Remove a value of a dice from the ArrayList to be unbanked
-	 * @param value int face value of die to be removed
-	 */
-	public void removeFromSingle(int die){
-		
-		boolean found = false;
-		int i = 0;
-		while(!found){
-			if(single.get(i)*-1==die){
-				single.remove(i);
-				found = true;
-			}
-			else
-				i++;
-		}
-	}
-	
-	/**
-	 * Clear all stored values of score and dice
-	 */
-	public void clear(){
-		score = 0;
-		single.clear();
-	}
-	
-	/**
-	 * Reset all values of game type to a new object
-	 */
-	public void reset(){
-		single.clear();
-	}
-	
 	
 	/**
 	 * Calculate the score of banked dice
@@ -130,30 +87,4 @@ public class Standard{
 	}
 	
 	
-	/**
-	 * Return the temporary value of the users score
-	 * @return
-	 */
-	public int getTemp(){
-		return tempScore;
-	}
-	
-	/**
-	 * Return the overall value of the users score
-	 * @return
-	 */
-	public int getScore(){
-		return score;
-	}
-	
-	
-	/**
-	 * Return if the values stored in bank are valid
-	 * @return
-	 */
-	public boolean isChange(){
-		return change;
-	}
-
-
 }
