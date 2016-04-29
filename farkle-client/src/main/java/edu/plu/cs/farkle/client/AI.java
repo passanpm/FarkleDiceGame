e6749f0083624917a339e3680ee290bc41b844f8
unknown <passanpm@CSCE18052.cs.PLU.EDU>
2016-04-28 20:24:36 -0700

    Added to AI.java

--- farkle-client/src/main/java/edu/plu/cs/farkle/client/AI.java
+++ farkle-client/src/main/java/edu/plu/cs/farkle/client/AI.java
@@ -19,23 +19,43 @@ public class AI {
 		die = new Dice();
 	}
 
-	//Picks best roll based on difficulty the player selected
+	/**
+	 * Rolls the dice and stores the result in diceRoll
+	 */
 	public void roll(){
 		diceRoll = die.getRoll();
-		for(int i = 0; i < diceRoll.size(); i++){
-			
-		}
-		
 	}
 	
-	private int bestRoll(int roll){
+	/**
+	 * Picks best roll based on difficulty the player selected
+	 * @return
+	 */
+	private int bestRoll(){
 		int score = 0;
 		
-		for(int i = 0; i < difficulty; i++){
+		for(int i = 0; i < diceRoll.size(); i++){
 			
 		}
 		
 		return score;
 	}
-	
+	/**
+	 * Checks to see if diceRoll has a set of threes and returns an array of the indexes of that set of three
+	 * @return int[] index the array of indexes
+	 */
+	private int[] setOfThrees(){
+		int[] index = new int[3];
+		ArrayList<Integer> check = new ArrayList<Integer>();
+		for(int i = 0; i< 6; i++){
+			check.add(i);
+			check.add(i);
+			check.add(i);
+			for(int j = 0; j < diceRoll.size(); j++){
+				if(diceRoll.containsAll(check)){
+					
+				}
+			}
+		}
+		return index;
+	}
 }
