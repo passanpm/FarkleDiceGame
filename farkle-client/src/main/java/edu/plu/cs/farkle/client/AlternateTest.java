fff3a250b623594ea79fee95d18f6b8ae34efefa
goldenzs <goldenzs@plu.edu>
2016-05-02 10:29:59 -0700

    Changed newGameplay to Alternate

---
+++ farkle-client/src/main/java/edu/plu/cs/farkle/client/AlternateTest.java
@@ -0,0 +1,19 @@
+package edu.plu.cs.farkle.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AlternateTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testBankScore() {
+		fail("Not yet implemented");
+	}
+
+}
