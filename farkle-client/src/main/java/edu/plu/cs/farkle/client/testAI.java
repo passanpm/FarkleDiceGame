25730451b3eb38047d8a6546256d2e34c656fab2
passanpm <passanpm@plu.edu>
2016-05-02 10:29:53 -0700

    moved testAI.java

---
+++ farkle-client/src/main/java/edu/plu/cs/farkle/client/testAI.java
@@ -0,0 +1,19 @@
+package edu.plu.cs.farkle.client;
+import edu.plu.cs.farkle.client.AI;
+
+public class testAI {
+
+
+	public static void main(String[] args) {
+		AI hauser = new AI(1, "hauser");
+		System.out.println("Hello, my name is " + hauser.getName() + " and my difficulty is " + hauser.getDifficulty());
+		hauser.roll();
+		
+		int[] indices = new int[3];
+		indices = hauser.setOfThrees();
+		
+		for(int i = 0; i< indices.length; i++)
+			System.out.print(indices[i] + ",");
+	}
+
+}
