808b3fe72980336d8a35871a91c444605365ffc0
goldenzs <goldenzs@plu.edu>
2016-05-02 11:34:59 -0700

    Testing for Alternate

--- farkle-client/src/main/java/edu/plu/cs/farkle/client/AlternateTest.java
+++ farkle-client/src/main/java/edu/plu/cs/farkle/client/AlternateTest.java
@@ -6,14 +6,47 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class AlternateTest {
+	
+	
+	Alternate test = new Alternate();
 
-	@Before
-	public void setUp() throws Exception {
-	}
 
 	@Test
 	public void testBankScore() {
-		fail("Not yet implemented");
+		int value;
+		
+		test.addToSingle(1);
+		value = test.bankScore();
+		System.out.println("Test One Scoring: ");
+		test.getNum();
+		assertEquals(100, value);
+		
+		test.clear();
+		test.reset();
+		test.addToSingle(1);
+		test.addToSingle(2);
+		test.addToSingle(3);
+		test.addToSingle(4);
+		test.addToSingle(5);
+		test.addToSingle(6);
+		value = test.bankScore();
+		System.out.println("Test Straight Scoring: ");
+		test.getNum();
+		assertEquals(2500, value);
+		
+		test.clear();
+		test.reset();
+		test.addToSingle(2);
+		test.addToSingle(2);
+		test.addToSingle(2);
+		test.addToSingle(2);
+		test.addToSingle(2);
+		test.addToSingle(2);
+		value = test.bankScore();
+		System.out.println("Test Six of a Kind:");
+		test.getNum();
+		assertEquals(6000, value);		
 	}
 
+	
 }
