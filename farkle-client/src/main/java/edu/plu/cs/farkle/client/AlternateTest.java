0499346304b77aa7b24cdfe492d4124ab92a4091
goldenzs <goldenzs@plu.edu>
2016-05-02 11:59:46 -0700

    Added Output for Testing

--- farkle-client/src/main/java/edu/plu/cs/farkle/client/AlternateTest.java
+++ farkle-client/src/main/java/edu/plu/cs/farkle/client/AlternateTest.java
@@ -7,9 +7,7 @@ import org.junit.Test;
 
 public class AlternateTest {
 	
-	
 	Alternate test = new Alternate();
-
 
 	@Test
 	public void testBankScore() {
@@ -46,6 +44,67 @@ public class AlternateTest {
 		System.out.println("Test Six of a Kind:");
 		test.getNum();
 		assertEquals(6000, value);		
+		
+		test.clear();
+		test.reset();
+		test.addToSingle(3);
+		test.addToSingle(3);
+		test.addToSingle(3);
+		test.addToSingle(3);
+		test.addToSingle(3);
+		value = test.bankScore();
+		System.out.println("Test Five of a Kind:");
+		test.getNum();
+		assertEquals(4000, value);
+		
+		test.clear();
+		test.reset();
+		test.addToSingle(4);
+		test.addToSingle(4);
+		test.addToSingle(4);
+		test.addToSingle(4);
+		value = test.bankScore();
+		System.out.println("Test Four of a Kind:");
+		test.getNum();
+		assertEquals(2000, value);
+		
+		test.clear();
+		test.reset();
+		test.addToSingle(2);
+		test.addToSingle(2);
+		test.addToSingle(2);
+		value =  test.bankScore();
+		System.out.println("Set of Three");
+		test.getNum();
+		assertEquals(200, value);
+		
+		/*
+		test.clear();
+		test.reset();
+		test.addToSingle(2);
+		test.addToSingle(2);
+		test.addToSingle(3);
+		test.addToSingle(3);
+		test.addToSingle(6);
+		test.addToSingle(6);
+		value =  test.bankScore();
+		System.out.println("Test Three Pair");
+		test.getNum();
+		assertEquals(1500, value);
+		
+		test.clear();
+		test.reset();
+		test.addToSingle(2);
+		test.addToSingle(3);
+		test.addToSingle(4);
+		test.addToSingle(3);
+		test.addToSingle(6);
+		test.addToSingle(6);
+		value =  test.bankScore();
+		System.out.println("Test No Scoring Dice");
+		test.getNum();
+		assertEquals(500, value);
+		*/
 	}
 
 	
