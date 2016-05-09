50f7d88c4bd88793f19a149cf021137f86007480
passanpm <passanpm@plu.edu>
2016-05-09 11:30:35 -0700

    testAI edits

--- farkle-client/src/main/java/edu/plu/cs/farkle/client/testAI.java
+++ farkle-client/src/main/java/edu/plu/cs/farkle/client/testAI.java
@@ -9,11 +9,11 @@ public class testAI {
 		
 		System.out.println("Hello, my name is " + a.getName() + " and my difficulty is " + a.getDifficulty());
 		
-		System.out.println(a.roll().toString());
+		System.out.println("Roll: " + a.roll().toString());
 		
-		System.out.println(a.getIndexes().toString());
+		System.out.println("Indexes: " + a.getIndexes().toString());
 		
-		System.out.println(a.getScore().toString());
+		System.out.println("Scoring Dice: " + a.getScore().toString());
 	}
 	
 	
